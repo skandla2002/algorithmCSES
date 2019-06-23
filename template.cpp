7a39e8c46fb8df056e93a3e5fcba6cc3e69bd9e9
skandla2002 <skandla2002@gmail.com>
2019-06-23 23:38:35 +0900

    template

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std; // std::cout 아닌 cout 사용가능 g++ -std=c++11 -02 -Wall template.cpp -o template => template라는 실행파일 생성함 -02(코드 최적화), -std=c++11(C++11 표준 컴파일러), -Wall(발생가능한오류에 대한 경고 띄워줌
+
+int main() {
+	// 이부분에 풀이를 작성한다.
+	int a, b;
+	string x;
+	cin >> a >> b >> x;
+	//123 456 monkey // 공백 문자만 존재
+	//123 456
+	//monkey  // 공백과 개행문자 존재
+	int a = 123, b = 456;
+	string x = "monkey";
+	cocut << a << " " << b << " " << x << "\n";
+}
+// 입력 : cin 
+// 출력 : cout
+// scanf, printf
