343556411511fe26da3d55df5a8216d7f1b3e373
skandla2002 <skandla2002@gmail.com>
2019-07-03 00:00:47 +0900

    알고리즘 설계기법: 비트병렬알고리즘

---
+++ algorithm_design.cpp
@@ -0,0 +1,18 @@
+// 비트 병렬 알고리즘
+// 해밍거리 : 길이가 같은 두 문자열 a, b 사이의 해밍거리는 두문자열이 일치하지 않는 위치의 개수이다.
+hamming(01101, 11001) = 2;
+hamming(00111, 11110) = 3;
+hamming(01101, 11110) = 2;
+//O(n^2k) 시간
+int hamming(string a, string b){
+    int d = 0;
+    for(int i = 0; i < k ; i++){
+        if(a[i] != b[i]) d++;
+    }
+    return d;
+}
+
+// 비트로 구성함을 최적화(기존 알고리즘보다 20배 빠름)
+int hamming(int a, int b){
+    return __builtin_popcount(a^b);
+}
