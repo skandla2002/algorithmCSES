d0ad1e80bb3a4b68bda18192585a81ed70b0f0f9
skandla2002 <skandla2002@gmail.com>
2019-07-23 23:28:51 +0900

    class 파일 커밋 제외, CCW 로직 구현중

--- java/battle/Solution.java
+++ java/battle/Solution.java
@@ -1,6 +1,7 @@
 package battle;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -10,31 +11,66 @@ public class Solution {
 	static int N;
 	static int Min;
 	static int[] Result;
-
+	static Point[][] Temp;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		FileReader fr = new FileReader("D:/Developer/Source/SWTest/src/battle/input.txt");
+		BufferedReader br = new BufferedReader(fr);
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		int T = Integer.parseInt(br.readLine());
 		for(int i = 0 ; i < T ; i++){
-			st = new StringTokenizer(br.readLine());
-			Point p1 = new Point(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
-			st = new StringTokenizer(br.readLine());
-			Point p2 = new Point(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
-			st = new StringTokenizer(br.readLine());
-			Point p3 = new Point(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
+			N = Integer.parseInt(br.readLine());
+			Result = new int[N]; 
+			Temp = new Point[N][2];
+			for(int j = 0 ; j < N ; j++){
+				st = new StringTokenizer(br.readLine());
+				Temp[j][0] = new Point(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
+				Temp[j][1] = new Point(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
+				System.out.println(Temp[j][0].x + " " + Temp[j][0].y + " " + Temp[j][1].x + " " + Temp[j][1].y);
+			}
 			
-			System.out.println(ccw(p1, p2, p3));
+			// 시간내 처리 방식 확인
+			for(int Min = 0 ; Min < 31; Min++){
+				for(int k = 0 ; k < N ; k ++ ){
+					int ccwResult = ccw(Temp[k][0], Temp[k][1], Temp[k+1][0]);
+					if (ccwResult == 0){
+						
+					}
+				}
+			}
 			
+			
+//			System.out.println(ccw(p1, p2, p3));
+			
+			
+			// 결과 출력
+			System.out.println("#" + (i + 1) + makeInt(Result));
 		}
+		
 	}
 	
 	static int ccw(Point p1, Point p2, Point p3){
 		long tmp = (p1.x * p2.y + p2.x * p3.y + p3.x * p1.y) - (p1.y * p2.x + p2.y * p3.x + p3.y * p1.x);
-		if(tmp < 0) return -1;
-		else if(tmp > 0) return 1;
+		if(tmp > 0) return 1;
+		else if(tmp < 0) return -1;
 		else return 0;
+	}
+	
+//	static int ccw(Point p1, Point p2, Point p3){
+//		long tmp = (p1.x1 * p2.y + p2.x * p3.y + p3.x * p1.y) - (p1.y * p2.x + p2.y * p3.x + p3.y * p1.x);
+//		if(tmp < 0) return -1;
+//		else if(tmp > 0) return 1;
+//		else return 0;
+//	}
+	
+	static String makeInt(int[] Result){
+		String resultStr = "";
+		for(int i = 0 , len = Result.length ; i < len ; i++){
+			resultStr = resultStr + " " + Result[i];
+		}
+		return resultStr;
 	}
 
 }
